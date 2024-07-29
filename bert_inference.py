89d2281c45d1ddd844fbe213b553b2bf8701e509
Jin, Zixian <zixian.jin@intel.com>
2024-07-29 10:02:44 +0800

    modified bert_inference.py to OOP style

--- bert_inference.py
+++ bert_inference.py
@@ -1,8 +1,9 @@
 import torch
 from datasets import load_dataset
 from transformers import BertTokenizer, BertModel, AdamW
+from torch.utils.data import DataLoader, Dataset
 
-class Dataset(torch.utils.data.Dataset):
+class BertDataset(Dataset):
     def __init__(self, split):
         self.dataset = load_dataset(path='lansinuote/ChnSentiCorp', split=split)
 
@@ -12,146 +13,140 @@ class Dataset(torch.utils.data.Dataset):
     def __getitem__(self, i):
         text = self.dataset[i]['text']
         label = self.dataset[i]['label']
-
         return text, label
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-dataset = Dataset('train')
-# len(dataset), dataset[0]
-token = BertTokenizer.from_pretrained('bert-base-chinese')
-pretrained = BertModel.from_pretrained('bert-base-chinese')
-pretrained.to(device)
-# pretrained = BertModel.from_pretrained('tmp')  # local checkpoint
-#不训练,不需要计算梯度
-for param in pretrained.parameters():
-    param.requires_grad_(False)
+
     
-def collate_fn(data):
-    sents = [i[0] for i in data]
-    labels = [i[1] for i in data]
-
-    #编码
-    data = token.batch_encode_plus(batch_text_or_text_pairs=sents,
-                                   truncation=True,
-                                   padding='max_length',
-                                   max_length=500,
-                                   return_tensors='pt',
-                                   return_length=True)
-
-    #input_ids:编码之后的数字
-    #attention_mask:是补零的位置是0,其他位置是1
-    input_ids = data['input_ids'].to(device)
-    attention_mask = data['attention_mask'].to(device)
-    token_type_ids = data['token_type_ids'].to(device)
-    labels = torch.LongTensor(labels).to(device)
-    #print(data['length'], data['length'].max())
-
-    return input_ids, attention_mask, token_type_ids, labels
-
-loader = torch.utils.data.DataLoader(dataset=dataset,
-                                     batch_size=16,
-                                     collate_fn=collate_fn,
-                                     shuffle=True,
-                                     drop_last=True)
-
-for i, (input_ids, attention_mask, token_type_ids,
-        labels) in enumerate(loader):
-    break
 # print(len(loader))
 # input_ids.shape, attention_mask.shape, token_type_ids.shape, labels
-
-
-
-
 #模型试算
 # out = pretrained(input_ids=input_ids,
 #            attention_mask=attention_mask,
 #            token_type_ids=token_type_ids)
 # out.last_hidden_state.shape
 
-# Define a model for downstream task 
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu' # TODO
+
+
+
 class DownstreamModel(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        # print(len(dataset), dataset[0])
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-chinese')
+        self.pretrained = BertModel.from_pretrained('bert-base-chinese')
+        self.pretrained.to(device) # TODO
+            # pretrained = BertModel.from_pretrained('tmp')  # local checkpoint
+        # inactivate grad for Bert params
+        for param in self.pretrained.parameters():
+            param.requires_grad_(False)
+
+        self.init_dataset()
+        self.init_dataloader()
         self.fc1 = torch.nn.Linear(768, 192)
         self.fc2 = torch.nn.Linear(192, 2)
+    
+    def init_dataset(self):
+        self.train_dataset = BertDataset('train')
+        self.val_dataset = BertDataset('test')
 
+    def init_dataloader(self):
+        def collate_fn(data):
+            tokenizer = self.tokenizer
+            sents = [i[0] for i in data]
+            labels = [i[1] for i in data]
+
+            # encoding
+            data = tokenizer.batch_encode_plus(batch_text_or_text_pairs=sents,
+                                        truncation=True,
+                                        padding='max_length',
+                                        max_length=500,
+                                        return_tensors='pt',
+                                        return_length=True)
+            #input_ids: ids after encoding
+            input_ids = data['input_ids'].to(device)
+            attention_mask = data['attention_mask'].to(device)
+            token_type_ids = data['token_type_ids'].to(device)
+            labels = torch.LongTensor(labels).to(device)
+            #print(data['length'], data['length'].max())
+
+            return input_ids, attention_mask, token_type_ids, labels
+
+        self.train_dataloader = DataLoader(dataset=self.train_dataset,
+                                            batch_size=16,
+                                            collate_fn=collate_fn,
+                                            shuffle=True,
+                                            drop_last=True)
+        self.val_dataloader = DataLoader(dataset=self.val_dataset,
+                                                batch_size=32,
+                                                collate_fn=collate_fn,
+                                                shuffle=True,
+                                                drop_last=True)
+        for i, (input_ids, attention_mask, token_type_ids,
+                labels) in enumerate(self.train_dataloader):
+            break
+        
     def forward(self, input_ids, attention_mask, token_type_ids):
         with torch.no_grad():
-            out = pretrained(input_ids=input_ids,
+            out = self.pretrained(input_ids=input_ids,
                        attention_mask=attention_mask,
                        token_type_ids=token_type_ids)
-
         out = self.fc1(out.last_hidden_state[:, 0])
         out = self.fc2(out)
         out = out.softmax(dim=1)
-
         return out
 
+    def downstream_train(self):
+        optimizer = AdamW(model.parameters(), lr=5e-4)
+        criterion = torch.nn.CrossEntropyLoss()
 
-model = DownstreamModel()
-model.to(device)
+        model.train()
+        for i, (input_ids, attention_mask, token_type_ids, labels) in enumerate(self.train_dataloader):
+            out = model(input_ids=input_ids,
+                        attention_mask=attention_mask,
+                        token_type_ids=token_type_ids)
+            loss = criterion(out, labels)
+            loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            if i % 5 == 0:
+                out = out.argmax(dim=1)
+                accuracy = (out == labels).sum().item() / len(labels)
+                print(i, loss.item(), accuracy)
+            if i == 300:
+                break
+        
+    def downstream_test(self):
+        model.eval()
+        correct = 0
+        total = 0
+
+        for i, (input_ids, attention_mask, token_type_ids,
+                labels) in enumerate(self.val_dataloader):
+            if i == 5:
+                break
+            print(i)
+            with torch.no_grad():
+                out = model(input_ids=input_ids,
+                            attention_mask=attention_mask,
+                            token_type_ids=token_type_ids)
+
+            out = out.argmax(dim=1)
+            correct += (out == labels).sum().item()
+            total += len(labels)
+        print(correct / total)
+
+
 # model(input_ids=input_ids,
 #       attention_mask=attention_mask,
 #       token_type_ids=token_type_ids).shape
 
 
-def train():
-    optimizer = AdamW(model.parameters(), lr=5e-4)
-    criterion = torch.nn.CrossEntropyLoss()
-
-    model.train()
-    for i, (input_ids, attention_mask, token_type_ids, labels) in enumerate(loader):
-        out = model(input_ids=input_ids,
-                    attention_mask=attention_mask,
-                    token_type_ids=token_type_ids)
-
-        loss = criterion(out, labels)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if i % 5 == 0:
-            out = out.argmax(dim=1)
-            accuracy = (out == labels).sum().item() / len(labels)
-
-            print(i, loss.item(), accuracy)
-
-        if i == 300:
-            break
-    
-    
-
-def test():
-    model.eval()
-    correct = 0
-    total = 0
-
-    loader_test = torch.utils.data.DataLoader(dataset=Dataset('validation'),
-                                              batch_size=32,
-                                              collate_fn=collate_fn,
-                                              shuffle=True,
-                                              drop_last=True)
-
-    for i, (input_ids, attention_mask, token_type_ids,
-            labels) in enumerate(loader_test):
-
-        if i == 5:
-            break
-
-        print(i)
-
-        with torch.no_grad():
-            out = model(input_ids=input_ids,
-                        attention_mask=attention_mask,
-                        token_type_ids=token_type_ids)
-
-        out = out.argmax(dim=1)
-        correct += (out == labels).sum().item()
-        total += len(labels)
-
-    print(correct / total)
 
 if __name__ == '__main__':
-    train()
-    test()
+    model = DownstreamModel()
+    model.to(device)
+    model.downstream_train()
+    model.downstream_test()
