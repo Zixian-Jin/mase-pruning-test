002da1db1f658ffd14a864681e1f4afaecbb3481
Jin, Zixian <zixian.jin@intel.com>
2024-08-23 16:37:29 +0800

    sync: updated analyse_data.py

--- analyse_data.py
+++ analyse_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import re
 
-def analyse_sensitivity_test_log(log_path, start_line, end_line) -> dict:
+def analyse_sensitivity_test_type1_log(log_path, start_line, end_line) -> dict:
     data = {
         'Layer Pruned': [],
         'Block Num': [],
@@ -37,10 +37,54 @@ def analyse_sensitivity_test_log(log_path, start_line, end_line) -> dict:
             # print(f'Layer={layer}, block_num={block_num}, sp={sp}, acc.={acc}')
     f.close()
     
-    print(f'Finished extracting {count} pieces of trial data.')
+    print(f'Finished extracting {len(data['Acc.'])} pieces of trial data.')
     df = pd.DataFrame(data)
     xlsx_path = log_path.rstrip('.log') + '.xlsx'
     df.to_excel(xlsx_path, index=False)
+
+def analyse_sensitivity_test_type2_log(log_path, start_line, end_line) -> dict:
+    data = {
+        'Module Pruned': [],
+        'Block Num': [],
+        'Sparsity': [],
+        'Acc.': []
+    }
+    
+    f = open(log_path, 'r', encoding='utf-8')
+    count = 0
+    while True:
+        line = f.readline()
+        count += 1
+        if count <= start_line:
+            continue
+        if count > end_line:
+            break
+        if line == None:
+            break
+        if line.startswith('Module type pruned'):
+            # extract cfg
+            matches = re.findall(r":\s*([\d.]+)", line)
+            module = line.split(',')[0].split('= ')[-1]
+            block_num, sp = matches[0], matches[1]
+            # extract acc
+            acc_line = f.readline()
+            assert acc_line.startswith('Before pruning'), "did not catch an accuracy report"
+            matches = re.findall(r"After pruning: acc=([\d.]+)", acc_line)
+            acc = matches[0]
+            # append data
+            data['Module Pruned'].append(module)
+            data['Block Num'].append(block_num)
+            data['Sparsity'].append(sp)
+            data['Acc.'].append(acc)
+            # print(f'Layer={layer}, block_num={block_num}, sp={sp}, acc.={acc}')
+    f.close()
+    
+    print(f'Finished extracting {len(data['Acc.'])} pieces of trial data.')
+    df = pd.DataFrame(data)
+    xlsx_path = log_path.rstrip('.log') + '.xlsx'
+    df.to_excel(xlsx_path, index=False)
+
         
 if __name__ == '__main__':
-    analyse_sensitivity_test_log('./logs/sensitivity_test_0817_1836.log', start_line=112, end_line=100000)
+    # analyse_sensitivity_test_type1_log('./logs/sensitivity_test_0817_1836.log', start_line=112, end_line=100000)
+    analyse_sensitivity_test_type2_log('./logs/sensitivity_test_type2_0823.log', start_line=0, end_line=100000)
